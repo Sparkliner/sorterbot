12f999ae05753cf53a5f1c28e0fd21bd26b9c43d
Felipe Reyes Miftajov <koslink@gmail.com>
2017-11-26 16:09:22 -0600

    updated test code

--- src/sbot_ik/scripts/talker.py
+++ src/sbot_ik/scripts/talker.py
@@ -4,14 +4,14 @@ import rospy
 from sbot_msg.msg import Position2D
 
 def talker():
-	pub = rospy.Publisher('chatter', Position2D, queue_size =10)
+	pub = rospy.Publisher('targetposition', Position2D, queue_size =10)
 	rospy.init_node('talker', anonymous=True)
 	rate = rospy.Rate(10)
 	outputData = Position2D()
 	while not rospy.is_shutdown():
 		outputData.x = 1.0
 		outputData.y = 2.0
-		rospy.loginfo(outputData)
+		#rospy.loginfo(outputData)
 		pub.publish(outputData)
 		rate.sleep()
 
--- src/sbot_ik/src/inverse_kine.cpp
+++ src/sbot_ik/src/inverse_kine.cpp
@@ -4,9 +4,29 @@
 
 #define PI 3.1414926535897932
 
+#define L1 25.4
+#define L2 25.4
+
 void calculateInverseKinematics(const sbot_msg::Position2D::ConstPtr& loc)
 {
+	double x, y, angle1, angle2;
 	ROS_INFO("Received: x=%f, y=%f", loc->x, loc->y);
+	
+	x = double(loc->x);
+	y = double(loc->y);
+
+	// law of cosines
+	angle2 = acos((pow(x,2)+pow(y,2)-pow(L1,2)-pow(L2,2))/(2*L1*L2));
+	angle1 = atan2(L2*sin(angle2)*x+(L1+L2*cos(angle2))*y,(L1+L2*cos(angle2))*x-L2*sin(angle2)*y);
+
+	//convert to degrees
+	angle1 = angle1*180.0/PI;
+	angle2 = angle2*180.0/PI;
+
+	ROS_INFO("Sending: theta1=%f degres, theta2=%f degrees", angle1, angle2);
+
+	
+
 }
 
 int main(int argc, char **argv)
@@ -17,7 +37,7 @@ int main(int argc, char **argv)
 	
 	ros::Subscriber sub = n.subscribe("targetposition", 1000, calculateInverseKinematics);
 	
-	ros::spin;
+	ros::spin();
 	
 	return 0;
 }
