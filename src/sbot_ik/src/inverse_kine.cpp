7e011927a238490348dd3bef351c28a851a26991
Felipe Reyes Miftajov <koslink@gmail.com>
2017-11-29 17:07:10 -0600

    refactored inverse_kine to pubish as well as subscribe

--- src/sbot_ik/src/inverse_kine.cpp
+++ src/sbot_ik/src/inverse_kine.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "sbot_msg/Position2D.h"
+#include "sbot_msg/JointTarget.h"
 #include <math.h>
 
 #define PI 3.1414926535897932
@@ -9,77 +10,97 @@
 
 #define ERR 10
 
-double wrapAngle(double angle) // constrain to 0-360
+class InverseKinematicSubPub
 {
-	angle = fmod(angle,360);
-	if (angle < 0)
+public:
+	InverseKinematicSubPub()
 	{
-		angle += 360;
+		angle_pub = n.advertise<sbot_msg::JointTarget>("servo_angle_target",1000);
+
+		sub = n.subscribe("targetposition", 1000, &InverseKinematicSubPub::calculateInverseKinematics,this);
 	}
 
-	return angle;
-}
-
-void calculateInverseKinematics(const sbot_msg::Position2D::ConstPtr& loc)
-{
-	double x, y, angle1, angle2;
-	ROS_INFO("Received: x=%f, y=%f", loc->x, loc->y);
-	
-	x = double(loc->x);
-	y = double(loc->y);
-
-	// law of cosines
-	angle2 = acos((pow(x,2)+pow(y,2)-pow(L1,2)-pow(L2,2))/(2*L1*L2));
-	angle1 = atan2(-L2*sin(angle2)*x+(L1+L2*cos(angle2))*y,(L1+L2*cos(angle2))*x+L2*sin(angle2)*y);
-
-	//convert to degrees and wrap to 0-360
-	angle1 = wrapAngle(angle1*180.0/PI);
-	angle2 = wrapAngle(angle2*180.0/PI);
-	
-
-	if (sqrt(pow(x,2)+pow(y,2)) > (L1+L2)) //test for out of reach conditions
+	double wrapAngle(double angle) // constrain to 0-360
 	{
-		ROS_INFO("Robot tried to reach too far");
-		angle1 = wrapAngle(atan2(y,x)*180.0/PI);
-		angle2 = ERR;
+		angle = fmod(angle,360);
+		if (angle < 0)
+		{
+			angle += 360;
+		}
+
+		return angle;
 	}
 
-	if (angle1 > 270)
+	void calculateInverseKinematics(const sbot_msg::Position2D::ConstPtr& loc)
 	{
-		ROS_INFO("Unreachable position (servo 1)");
-		angle1 = 270;
+		double x, y, angle1, angle2;
+		ROS_INFO("Received: x=%f, y=%f", loc->x, loc->y);
+		
+		x = double(loc->x);
+		y = double(loc->y);
+
+		// law of cosines
+		angle2 = acos((pow(x,2)+pow(y,2)-pow(L1,2)-pow(L2,2))/(2*L1*L2));
+		angle1 = atan2(-L2*sin(angle2)*x+(L1+L2*cos(angle2))*y,(L1+L2*cos(angle2))*x+L2*sin(angle2)*y);
+
+		//convert to degrees and wrap to 0-360
+		angle1 = wrapAngle(angle1*180.0/PI);
+		angle2 = wrapAngle(angle2*180.0/PI);
+		
+
+		if (sqrt(pow(x,2)+pow(y,2)) > (L1+L2)) //test for out of reach conditions
+		{
+			ROS_INFO("Robot tried to reach too far");
+			angle1 = wrapAngle(atan2(y,x)*180.0/PI);
+			angle2 = ERR;
+		}
+
+		if (angle1 > 270)
+		{
+			ROS_INFO("Unreachable position (servo 1)");
+			angle1 = 270;
+		}
+
+
+		//avoid singularity conditions by using offset ERR
+		if (abs(angle2-180.0) < ERR)
+		{
+			ROS_INFO("Robot at fully bent singularity");
+			angle2 = 180.0 - ERR;
+		}
+
+		if (abs(angle2) < ERR)
+		{
+			ROS_INFO("Robot at fully extended singularity");
+			angle2 = ERR;
+		}
+
+		
+		ROS_INFO("Sending: theta1=%f degrees, theta2=%f degrees", angle1, angle2);
+
+		sbot_msg::JointTarget anglemsg;
+
+		anglemsg.joint1 = angle1;
+		anglemsg.joint2 = angle2;
+
+		angle_pub.publish(anglemsg);
+
 	}
 
+private:
+	ros::NodeHandle n;
+	ros::Publisher angle_pub;
+	ros::Subscriber sub;
 
-	//avoid singularity conditions by using offset ERR
-	if (abs(angle2-180.0) < ERR)
-	{
-		ROS_INFO("Robot at fully bent singularity");
-		angle2 = 180.0 - ERR;
-	}
-
-	if (abs(angle2) < ERR)
-	{
-		ROS_INFO("Robot at fully extended singularity");
-		angle2 = ERR;
-	}
-
-	
-	ROS_INFO("Sending: theta1=%f degrees, theta2=%f degrees", angle1, angle2);
-
-	
-
-}
+};
 
 int main(int argc, char **argv)
 {
 	ros::init(argc,argv,"inverse_kine");
+
+	InverseKinematicSubPub IKObject;
 	
-	ros::NodeHandle n;
-	
-	ros::Subscriber sub = n.subscribe("targetposition", 1000, calculateInverseKinematics);
-	
-	ros::spinOnce();
+	ros::spin();
 	
 	return 0;
 }
