dd4074d96bdd3142e7a55e8b86032670e3cd7739
Felipe Constantin Reyes Miftajov <koslink@gmail.com>
2017-11-30 13:39:36 -0600

    Now uses PWM library but values are all test values

--- src/sbot_ik/scripts/command_arm.py
+++ src/sbot_ik/scripts/command_arm.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 import rospy
 from sbot_msg.msg import JointTarget
-#import Adafruit_BBIO.PWM as PWM
+import Adafruit_BBIO.PWM as PWM
 
 def sendArmCommand(jtarget):
 	angle1 = jtarget.joint1
 	angle2 = jtarget.joint2
 	rospy.loginfo("Received angle1 =%f, angle2 =%f",angle1,angle2)
 	#calculate the proper duty cycle here
-	#PWM.start("P9_14", dutycyc1, freq = 2000, polarity = 0) #joint 1
-	#PWM.start("P9_16", dutycyc2, freq = 2000, polarity = 0) #joint 2
+	dutycyc1 = 25 #0 to 100
+	dutycyc2 = 50 #test different duty cycles
+	PWM.start("P9_14", dutycyc1, freq = 2000, polarity = 0) #joint 1
+	PWM.start("P9_16", dutycyc2, freq = 2000, polarity = 0) #joint 2
 
 def listener():
 	rospy.init_node('joint_target_listener', anonymous=True)
@@ -18,9 +20,9 @@ def listener():
 	rospy.spin()
 
 	#shutdown channels and cleanup
-	#PWM.stop("P9_14")
-	#PWM.stop("P9_16")
-	#PWM.cleanup()
+	PWM.stop("P9_14")
+	PWM.stop("P9_16")
+	PWM.cleanup()
 
 if __name__ == '__main__':
 	listener()
