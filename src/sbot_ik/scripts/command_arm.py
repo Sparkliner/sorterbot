ece46fe381d9bdd73da564140104da0b6dbf8435
Felipe Constantin Reyes Miftajov <koslink@gmail.com>
2017-12-01 13:44:02 -0600

    updated PWM code to calculate correct pulse width for desired angle

--- src/sbot_ik/scripts/command_arm.py
+++ src/sbot_ik/scripts/command_arm.py
@@ -3,16 +3,26 @@ import rospy
 from sbot_msg.msg import JointTarget
 import Adafruit_BBIO.PWM as PWM
 
+JOINT1MAX = 270
+JOINT2MAX = 180
+MINPULSE_MS = 0.5
+MAXPULSE_MS = 2.5
+SERVO_FREQUENCY = 50
+
+
+MINDUT = MINPULSE_MS*(SERVO_FREQUENCY/1000.0)*100
+MAXDUT = MAXPULSE_MS*(SERVO_FREQUENCY/1000.0)*100
+
 def sendArmCommand(jtarget):
 	angle1 = jtarget.joint1
 	angle2 = jtarget.joint2
 	rospy.loginfo("Received angle1 =%f, angle2 =%f",angle1,angle2)
 	#calculate the proper duty cycle here
-	dutycyc1 = 5 #2.5 to 12.5
-	dutycyc2 = 10 #test different duty cycles
-	#PWM.start("PIN",dutycycle,frequency,polarity(2 is inverted))
-	PWM.start("P9_14", dutycyc1, 50, 0) #joint 1
-	PWM.start("P9_16", dutycyc2, 50, 0) #joint 2
+	dutycyc1 = MINDUT + (MAXDUT-MINDUT)*(angle1/JOINT1MAX) #2.5 to 12.5
+	dutycyc2 = MINDUT + (MAXDUT-MINDUT)*(angle2/JOINT2MAX) #test different duty cycles
+	#PWM.start("PIN",dutycycle,frequency,polarity(1 is inverted))
+	PWM.start("P9_14", dutycyc1, SERVO_FREQUENCY, 0) #joint 1
+	PWM.start("P9_16", dutycyc2, SERVO_FREQUENCY, 0) #joint 2
 
 def listener():
 	rospy.init_node('joint_target_listener', anonymous=True)
