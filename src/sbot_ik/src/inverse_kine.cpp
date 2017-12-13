583bafcec20e10d450e06c2df79e0ee420342c10
Felipe Constantin Reyes Miftajov <koslink@gmail.com>
2017-12-12 21:53:14 -0600

    changed angle

--- src/sbot_ik/src/inverse_kine.cpp
+++ src/sbot_ik/src/inverse_kine.cpp
@@ -41,7 +41,7 @@ public:
 
 		// law of cosines
 		angle2 = acos((pow(x,2)+pow(y,2)-pow(L1,2)-pow(L2,2))/(2*L1*L2));
-		angle1 = atan2(-L2*sin(angle2)*x+(L1+L2*cos(angle2))*y,(L1+L2*cos(angle2))*x+L2*sin(angle2)*y);
+		angle1 = atan2(L2*sin(angle2)*x-(L1+L2*cos(angle2))*y,-(L1+L2*cos(angle2))*x-L2*sin(angle2)*y);
 
 		//convert to degrees and wrap to 0-360
 		angle1 = wrapAngle(angle1*180.0/PI);
